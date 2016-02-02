cbe0d07cef4c822009a7d99deba44ff205369c95
Mohammad <mohamad.kav@gmail.com>
2016-02-03 01:27:17 +0330

    Finally fixed the Spring bug :D

---
+++ src/net/hamtag/server/security/CsrfSecurityRequestMatcher.java
@@ -0,0 +1,21 @@
+package net.hamtag.server.security;
+
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+public class CsrfSecurityRequestMatcher implements RequestMatcher {
+    private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+    private RegexRequestMatcher unprotectedMatcher = new RegexRequestMatcher("/resource/.+", null);
+ 
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        if(allowedMethods.matcher(request.getMethod()).matches()){
+            return false;
+        }
+        return !unprotectedMatcher.matches(request);
+    }
+}
