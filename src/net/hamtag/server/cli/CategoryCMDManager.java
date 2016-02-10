26e1bc0d2bc0117538b4d30b449688851007b1cb
Mohammad <mohamad.kav@gmail.com>
2016-02-10 17:20:06 +0330

    Minor bugfix

--- src/net/hamtag/server/cli/CategoryCMDManager.java
+++ src/net/hamtag/server/cli/CategoryCMDManager.java
@@ -4,38 +4,62 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Scanner;
 
+import org.hibernate.Transaction;
+
 import net.hamtag.server.datatypes.category.Category;
 import net.hamtag.server.datatypes.category.CategoryMgr;
 
 public class CategoryCMDManager {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
-		Scanner input=new Scanner(System.in);
-		System.out.println("Choice 1 to add new Category, Choice 2 to add image to Category: ");
-		int choice=Integer.parseInt(input.nextLine());
-		if(choice==1){
-			System.out.println("Name: ");
-			Category category=new Category();
-			category.setName(input.nextLine());
-			System.out.println("In persian: ");
-			category.setTranslation(input.nextLine());
-			CategoryMgr.add(category);
-		}
-		else{
-			System.out.println("Category Name to add Image: ");
-			Category category=CategoryMgr.getCategoryByName(input.nextLine());
-			System.out.println("File Location: ");
-			File file=new File(input.nextLine());
-			byte[] bFile = new byte[(int) file.length()];
-			try {
-				FileInputStream fileInputStream = new FileInputStream(file);
-				fileInputStream.read(bFile);
-				fileInputStream.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+		Scanner input = new Scanner(System.in);
+		CategoryMgr.getCategoryByName("SPORTS");
+		while (true) {
+			System.out.println("\nChoice 1 to add or change Category, Choice 2 to add image to Category, or 3 to remove the Fucked up Category"
+					+ " and 4 to list all the Categories: ");
+			int choice = Integer.parseInt(input.nextLine());
+			if (choice == 1) {
+				System.out.println("Name: ");
+				String name=input.nextLine();
+				Category category=CategoryMgr.getCategoryByName(name);
+				if(category==null){
+					category = new Category();
+					category.setId(null);
+				}
+				category.setName(name);
+				System.out.println("In persian: ");
+				category.setTranslation(input.nextLine());
+				CategoryMgr.add(category);
+			} else if(choice==2){
+				System.out.println("Category Name to add Image: ");
+				Category category = CategoryMgr.getCategoryByName(input.nextLine());
+				System.out.println("File Location: ");
+				File file = new File(input.nextLine());
+				byte[] bFile = new byte[(int) file.length()];
+				try {
+					FileInputStream fileInputStream = new FileInputStream(file);
+					fileInputStream.read(bFile);
+					fileInputStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				category.setImage(bFile);
+				CategoryMgr.update(category);
 			}
-			category.setImage(bFile);
-			CategoryMgr.update(category);
+			else if(choice==3){
+				System.out.println("Category Name to DELETE: ");
+				Category category = CategoryMgr.getCategoryByName(input.nextLine());
+				Transaction tx = CategoryMgr.getInstance().beginTransaction();
+				Object o=CategoryMgr.getInstance().load(Category.class, category.getId());
+				CategoryMgr.getInstance().delete(o);
+				tx.commit();
+			}
+			else if(choice==4){
+				for(Category c:CategoryMgr.list()){
+					System.out.println(c.getName()+"\n  "+c.getTranslation());
+				}
+			}
+			System.out.println("Success");
 		}
 	}
 }
